ec3e6648ded584e563e36677685b5a8c1e6acf33
Gerzon05 <gerzonarletrangel@gmail.com>
2024-02-15 08:26:37 -0500

    💔 break style tailwind

--- src/Components/navbar.tsx
+++ src/Components/navbar.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import 'tailwindcss/components.css'
 import { navbar } from '@/utils/const'
 import Link from 'next/link'
 import { useState } from 'react'
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,11 @@ import Navbar from '@/Components/navbar'
 import { ButtonDefault } from '@/Components/ui'
 import principalimage from '../../public/princpalimage.png'
 import Image from 'next/image'
-import NavbarCopy from '@/Components/navbarcopy'
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <NavbarCopy />
       <div className='bg-black w-full h-[94vh] flex justify-between items-center'>
         <section className='md:w-1/2'></section>
         <section className='md:w-1/2'>
