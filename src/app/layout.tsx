066c49bc97e534b758374e7a968e9e244778cf2a
Gerzon05 <gerzonarletrangel@gmail.com>
2024-02-16 11:11:35 -0500

    🐛Fix the Idioms

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
+    <html lang='es'>
       <body>
         <main>{children}</main>
       </body>
