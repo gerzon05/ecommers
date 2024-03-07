9397aa4595d19f6ef6a9aa1c49f681b1c96bb5a4
Gerzon05 <gerzonarletrangel@gmail.com>
2024-03-07 09:45:10 -0500

    fix: fixed file postcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,26 @@
 /** @type {import('tailwindcss').Config} */
-import withMT from '@material-tailwind/react/utils/withMT'
+import withMT from "@material-tailwind/react/utils/withMT";
 
 module.exports = withMT({
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
     fontFamily: {
-      poppins: ['Poppins'],
+      poppins: ["Poppins"],
     },
     colors: {
-      'slate-one': '#4d041c',
-      'slate-two': '#fe6e8b',
-      'slate-three': '#e51951',
-      'slate-four': '#ffc0cb',
-      'slate-five': '#fff1f3',
-      'slate-six': '#ffc0cb',
+      "slate-one": "#4d041c",
+      "slate-two": "#fe6e8b",
+      "slate-three": "#e51951",
+      "slate-four": "#ffc0cb",
+      "slate-five": "#fff1f3",
+      "slate-six": "#ffc0cb",
     },
   },
   plugins: [],
-})
+});
